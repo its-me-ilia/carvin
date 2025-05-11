5be3c35c35d916c1e8ce6dd48f62c298a0a0c2e3
Tornike Topchishvili <torniketopchishvili@192.168.1.6>
2025-05-12 00:05:57 +0400

    back to check has been added

--- src/app/icons/ArrowIcon.tsx
+++ src/app/icons/ArrowIcon.tsx
@@ -2,12 +2,32 @@ export const ArrowIcon = () => {
   return (
     <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={24}
-    height={24}
-    fillRule="evenodd"
-    clipRule="evenodd"
+    width={36}
+    height={32}
+    viewBox="0 0 36 36"
+    fill="none"
   >
-    <path d="M2.117 12l7.527 6.235-.644.765-9-7.521 9-7.479.645.764-7.529 6.236h21.884v1h-21.883z" />
+    <g clipPath="url(#clip0_295_5)">
+      <path
+        d="M30.375 18H5.625"
+        stroke="#031424"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15.75 7.875L5.625 18L15.75 28.125"
+        stroke="#031424"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_295_5">
+        <rect width={36} height={36} fill="white" />
+      </clipPath>
+    </defs>
   </svg>
   );
 };
